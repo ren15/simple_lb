54eb809b69499ca7db35ecf10ee38f0b14c44d56
Ren Wang <iamwrm@gmail.com>
2022-08-25 14:05:54 +0000

    add ping-pong server

--- src/c1.py
+++ src/c1.py
@@ -1,22 +1,24 @@
+"""
+the client should be a ping-pong client
+
+"""
 import socket
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    host = socket.gethostname()
+    port = 5000
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
 
-    message = input(" -> ")  # take input
+    message = "ping"
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
+    for i in range(10):
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
 
         print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
 
