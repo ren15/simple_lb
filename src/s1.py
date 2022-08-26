18f49258be1c9bf9654afe35e94286f954fd456e
Ren Wang <iamwrm@gmail.com>
2022-08-26 02:08:53 +0000

    s1 use global variable

--- src/s1.py
+++ src/s1.py
@@ -1,16 +1,7 @@
 import socket
 from _thread import start_new_thread
 
-ServerSideSocket = socket.socket()
-host = '127.0.0.1'
-port = 2004
-ThreadCount = 0
-try:
-    ServerSideSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
-print('Socket is listening..')
-ServerSideSocket.listen(5)
+g_num = 0
 
 
 def multi_threaded_client(connection):
@@ -19,16 +10,35 @@ def multi_threaded_client(connection):
         data = connection.recv(2048).decode()
         if not data:
             break
-        print(data)
+        # print(data)
+        global g_num
+        g_num += 1
+        print(g_num)
         response = 'Server message: ' + str(int(data)+1)
         connection.sendall(str.encode(response))
     connection.close()
 
 
-for i in range(100):
-    Client, address = ServerSideSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(multi_threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSideSocket.close()
+def start_server_socket():
+    ServerSideSocket = socket.socket()
+    host = '127.0.0.1'
+    port = 2004
+    ThreadCount = 0
+    try:
+        ServerSideSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    print('Socket is listening..')
+    ServerSideSocket.listen(5)
+
+    for i in range(100):
+        Client, address = ServerSideSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        start_new_thread(multi_threaded_client, (Client, ))
+        ThreadCount += 1
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSideSocket.close()
+
+
+if __name__ == '__main__':
+    start_server_socket()
