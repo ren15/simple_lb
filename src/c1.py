ece83223673b70f7bf9fa1b0154f5efca3eebd51
Ren Wang <iamwrm@gmail.com>
2022-08-26 02:26:52 +0000

    c1 client socket to function

--- src/c1.py
+++ src/c1.py
@@ -1,20 +1,27 @@
 import socket
 import time
 
-ClientMultiSocket = socket.socket()
-host = '127.0.0.1'
-port = 2004
-print('Waiting for connection response')
-try:
-    ClientMultiSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
 
-res = ClientMultiSocket.recv(1024)
-for i in range(10):
-    Input = str(i)
-    ClientMultiSocket.send(str.encode(Input))
+def start_client_socket():
+
+    ClientMultiSocket = socket.socket()
+    host = '127.0.0.1'
+    port = 2004
+    print('Waiting for connection response')
+    try:
+        ClientMultiSocket.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+
     res = ClientMultiSocket.recv(1024)
-    print(res.decode('utf-8'))
-    time.sleep(1)
-ClientMultiSocket.close()
+    for i in range(10):
+        Input = str(i)
+        ClientMultiSocket.send(str.encode(Input))
+        res = ClientMultiSocket.recv(1024)
+        print(res.decode('utf-8'))
+        time.sleep(1)
+    ClientMultiSocket.close()
+
+
+if __name__ == '__main__':
+    start_client_socket()
